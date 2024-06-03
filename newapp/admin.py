cadd7bf4ecf3c87d92a782c03f0ec5fc60c49294
sreenandhna <156768245+sreenandhna@users.noreply.github.com>
2024-06-03 13:05:41 +0530

    Update admin.py

--- newapp/admin.py
+++ newapp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from . models import adminsignupmodel,Newbookmodel,studentsignupmodel,IssuedBook
+from .models import adminsignupmodel, Newbookmodel, studentsignupmodel, IssuedBook
 admin.site.register(adminsignupmodel)
 admin.site.register(Newbookmodel)
 admin.site.register(studentsignupmodel)
